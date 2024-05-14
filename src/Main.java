af1710c7a787b578129e00c87acd1206f0aba48b
XxXM4RI0XxX <montejanodiaza@gmail.com>
2024-05-14 08:38:58 -0600

    Added exeptions example

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        int x, y;
+
+        try {
+            System.out.print("First Number: ");
+            x = sc.nextInt();
+            sc.nextLine();
+
+            System.out.print("Second Number: ");
+            y = sc.nextInt();
+            sc.nextLine();
+
+            int result = x/y;
+            System.out.println("Division result: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("You can't divide by 0");
+            e.printStackTrace();
+        } catch (InputMismatchException e) {
+            System.out.println("Text is not number lol");
+            e.printStackTrace();
+        }
     }
 }
