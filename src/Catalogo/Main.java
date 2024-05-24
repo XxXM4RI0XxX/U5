662fa6006e8e361f431f4a94feabce323e5c08f0
XxXM4RI0XxX <montejanodiaza@gmail.com>
2024-05-24 13:05:31 -0600

    U5 project finished

---
+++ src/Catalogo/Main.java
@@ -0,0 +1,39 @@
+package Catalogo;
+
+import java.util.HashMap;
+
+import Catalogo.CustomExceptions.NegativeInputDetected;
+import Catalogo.CustomExceptions.ProductNotFound;
+import Catalogo.Util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        HashMap<Integer,Menu> menus = Seeder.getMenus();
+        Seeder.genProducts();
+
+        System.out.println("Bienvenido al catalogo :D!");
+        boolean flag = true;
+        do {
+        try {
+
+                System.out.println("\t>>>MENÚ<<<\n1) Añadir prodcuto\n2) Buscar prodcuto\n3) Remover producto\n0) Salir");
+                int opt = ConsoleReader.readInt();
+
+                if (opt != 0) {
+                    Menu menu = menus.get(opt);
+                    menu.execute();
+                } else flag = false;
+
+
+        }catch (IndexOutOfBoundsException | NullPointerException _){
+            System.out.println(">>> Invalid input...");
+        }catch (ProductNotFound | NegativeInputDetected _){}
+    } while (flag);
+        System.out.println("Adio :c");
+
+    }
+
+}
+
+
